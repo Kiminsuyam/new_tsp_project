17962da403cb7cafce1271fcad1f9d3ae5f51f53
TJ03 <tj03@TJ03ui-MacBookPro.local>
2021-11-03 11:21:49 +0900

    support entity validation 추가

--- src/main/java/com/tsp/new_tsp_project/api/admin/support/domain/entity/AdminSupportEntity.java
+++ src/main/java/com/tsp/new_tsp_project/api/admin/support/domain/entity/AdminSupportEntity.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.Date;
 
 @Entity
@@ -25,21 +26,26 @@ public class AdminSupportEntity {
 	private Integer idx;
 
 	@Column(name = "support_name")
+	@NotEmpty(message = "지원자 이름 입력은 필수입니다.")
 	private String supportName;
 
 	@Column(name = "support_height")
+	@NotEmpty(message = "지원자 키 입력은 필수입니다.")
 	private Integer supportHeight;
 
 	@Column(name = "support_size3")
+	@NotEmpty(message = "지원자 사이즈 입력은 필수입니다.")
 	private String supportSize3;
 
 	@Column(name = "support_instagram")
 	private String supportInstagram;
 
 	@Column(name = "support_phone")
+	@NotEmpty(message = "지원자 휴대폰 번호 입력은 필수입니다.")
 	private String supportPhone;
 
 	@Column(name = "support_message")
+	@NotEmpty(message = "지원자 상세 내용 입력은 필수입니다.")
 	@Lob
 	private String supportMessage;
 
